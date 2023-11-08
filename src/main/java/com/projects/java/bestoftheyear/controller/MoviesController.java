ae1bd46532c2d685c6f311f7cc39e50cfda7c26b
Paolo Falco <paolo.falcoapp@gmail.com>
2023-11-08 17:37:22 +0100

    Commit 8  - add Movie author and description

--- src/main/java/com/projects/java/bestoftheyear/controller/MoviesController.java
+++ src/main/java/com/projects/java/bestoftheyear/controller/MoviesController.java
@@ -31,9 +31,33 @@ public class MoviesController {
     // METODI PRIVATE
     private List<Movie> getBestMovies(){
         Movie[] moviesArray = {
-            new Movie(1, "Matrix"),
-            new Movie(2, "Terminator"),
-            new Movie(3, "Lo Hobbit")};
+            new Movie(
+                      1,
+                      "Matrix",
+                      "Lana Wachowski, Lilly Wachowski",
+                      "Matrix (The Matrix) è un film di fantascienza del 1999 in stile cyberpunk " +
+                                "scritto e diretto dai fratelli Andy e Larry Wachowski."),
+            new Movie(
+                      2,
+                      "Terminator",
+                      "James Cameron",
+                      "La trama fantascientifica, scritta da Cameron con Gale Anne Hurd, è incentrata " +
+                                "sul personaggio del titolo, un cyborg assassino (Arnold Schwarzenegger) " +
+                                "inviato indietro nel tempo dal 2029 al 1984 per uccidere Sarah Connor " +
+                                "(Linda Hamilton), il cui figlio un giorno diventerà un salvatore contro le macchine " +
+                                "in un futuro postapocalittico. Michael Biehn interpreta Kyle Reese, un soldato del " +
+                                "futuro inviato anch'egli indietro nel tempo per proteggere Sarah."),
+            new Movie(
+                      3,
+                      "Lo Hobbit",
+                      "Peter Jackson",
+                      "Lo Hobbit (The Hobbit) è una trilogia cinematografica fantasy co-prodotta, " +
+                                "co-scritta e diretta dal regista neozelandese Peter Jackson, basata sull'omonimo " +
+                                "romanzo scritto da J. R. R. Tolkien nel 1937, con diverse parti tratte dalle " +
+                                "Appendici de Il ritorno del re, ultimo libro della saga del Il Signore degli  " +
+                                "Anelli e da altri scritti. La trilogia è un prequel della trilogia del " +
+                                "Il Signore degli Anelli di Jackson, ed è formata da Un viaggio inaspettato (2012), " +
+                                "La desolazione di Smaug (2013) e La battaglia delle cinque armate (2014).")};
         return Arrays.asList(moviesArray);
     }
 
--- src/main/java/com/projects/java/bestoftheyear/model/Movie.java
+++ src/main/java/com/projects/java/bestoftheyear/model/Movie.java
@@ -4,10 +4,14 @@ public class Movie {
 
     private int id;
     private String title;
+    private String author;
+    private String description;
 
-    public Movie(int id, String title) {
+    public Movie(int id, String title, String author, String description) {
         this.id = id;
         this.title = title;
+        this.author = author;
+        this.description = description;
     }
 
     public int getId() {
@@ -18,11 +22,27 @@ public class Movie {
         return title;
     }
 
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
     public void setId(int id) {
         this.id = id;
     }
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
