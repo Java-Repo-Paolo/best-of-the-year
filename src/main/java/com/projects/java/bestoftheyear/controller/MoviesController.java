00e3311174f5aa550abdc2b2f935c4f6520893d7
Paolo Falco <paolo.falcoapp@gmail.com>
2023-11-08 17:04:01 +0100

    Commit 6  - add Movie details

--- src/main/java/com/projects/java/bestoftheyear/controller/MoviesController.java
+++ src/main/java/com/projects/java/bestoftheyear/controller/MoviesController.java
@@ -4,6 +4,7 @@ import com.projects.java.bestoftheyear.model.Movie;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Arrays;
@@ -19,6 +20,13 @@ public class MoviesController {
         return "movies";
     }
 
+    @GetMapping("/{id}")
+    public String MovieDetails(@PathVariable("id") int movieId, Model model) {
+        Movie movie = getMovieById(movieId);
+        model.addAttribute("movie", movie);
+        return "movie-details";
+    }
+
 
     // METODI PRIVATE
     private List<Movie> getBestMovies(){
@@ -27,6 +35,15 @@ public class MoviesController {
             new Movie(2, "Terminator"),
             new Movie(3, "Lo Hobbit")};
         return Arrays.asList(moviesArray);
+    }
+
+    private Movie getMovieById(int id) {
+        for (Movie movie : getBestMovies()) {
+            if (movie.getId() == id) {
+                return movie;
+            }
+        }
+        return null;
     }
 
 }
