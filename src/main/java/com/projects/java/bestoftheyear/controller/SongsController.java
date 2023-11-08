7f1711010de0ee460630c4221291b53ebb4cfe71
Paolo Falco <paolo.falcoapp@gmail.com>
2023-11-08 17:35:32 +0100

    Commit 7  - add Song details

--- src/main/java/com/projects/java/bestoftheyear/controller/SongsController.java
+++ src/main/java/com/projects/java/bestoftheyear/controller/SongsController.java
@@ -5,6 +5,7 @@ import com.projects.java.bestoftheyear.model.Song;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Arrays;
@@ -21,6 +22,13 @@ public class SongsController {
         return "songs";
     }
 
+    @GetMapping("/{id}")
+    public String SongDetails(@PathVariable("id") int movieId, Model model) {
+        Song song = getSongById(movieId);
+        model.addAttribute("song", song);
+        return "song-details";
+    }
+
     // METODI PRIVATE
     private List<Song> getBestSongs(){
         Song[] songsArray = {
@@ -28,5 +36,14 @@ public class SongsController {
                 new Song(2, "The End"),
                 new Song(3, "The Resistance")};
         return Arrays.asList(songsArray);
+    }
+
+    private Song getSongById(int id) {
+        for (Song song : getBestSongs()) {
+            if (song.getId() == id) {
+                return song;
+            }
+        }
+        return null;
     }
 }
