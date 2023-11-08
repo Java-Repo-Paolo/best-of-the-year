b377beb9998fae2328881e2fb6eea7792c4f727e
Paolo Falco <paolo.falcoapp@gmail.com>
2023-11-08 15:00:54 +0100

    Commit 5  - add Movies and Songs Model

---
+++ src/main/java/com/projects/java/bestoftheyear/model/Movie.java
@@ -0,0 +1,28 @@
+package com.projects.java.bestoftheyear.model;
+
+public class Movie {
+
+    private int id;
+    private String title;
+
+    public Movie(int id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
---
+++ src/main/java/com/projects/java/bestoftheyear/model/Song.java
@@ -0,0 +1,27 @@
+package com.projects.java.bestoftheyear.model;
+
+public class Song {
+    private int id;
+    private String title;
+
+    public Song(int id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
